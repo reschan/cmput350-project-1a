336e386ef6d1c50ad83c326f7de3003baf53a8d1
kcstarlight7 <fiona112.chuang@gmail.com>
2026-09-21 16:27:13 -0600

    implement drawcontext.cpp

--- engine/DrawContext.cpp
+++ engine/DrawContext.cpp
@@ -7,11 +7,28 @@ DrawContext::DrawContext(std::shared_ptr<sf::RenderWindow> window, std::shared_p
 
 void DrawContext::DrawCenteredText(const std::string &text, int pixelSize, Point2D p, RGBColor c) {}
 
-void DrawContext::DrawText(const std::string &text, int pixelSize, Point2D p, RGBColor c) {}
+void DrawContext::DrawText(const std::string &text, int pixelSize, Point2D p, RGBColor c) {
+    sf::Text textstring(*mFont);
+    textstring.setString(text);
+    textstring.setCharacterSize(pixelSize);
+    textstring.setFillColor(sf::Color::Color(c.r, c.g, c.b));
+    textstring.setPosition({p.x, p.y});
+    mWindow->draw(textstring);
+}
 
-void DrawContext::DrawCircle(Point2D p, float radius, RGBColor c) {}
+void DrawContext::DrawCircle(Point2D p, float radius, RGBColor c) {
+    sf::CircleShape circle(radius);
+    circle.setFillColor(sf::Color::Color(c.r, c.g, c.b));
+    circle.setPosition({p.x, p.y});
+    mWindow->draw(circle);
+}
 
-void DrawContext::DrawRect(Rect r, RGBColor c) {}
+void DrawContext::DrawRect(Rect r, RGBColor c) {
+    sf::RectangleShape rectangle({r.width, r.height});
+    rectangle.setFillColor(sf::Color::Color(c.r, c.g, c.b));
+    rectangle.setPosition({r.topLeft.x, r.topLeft.y});
+    mWindow->draw(rectangle);
+}
 
 void DrawContext::FrameRect(Rect r, float width, RGBColor c) {}
 
@@ -27,7 +44,16 @@ void DrawContext::FrameRect(Rect r, float width, RGBColor c) {}
  * and uses a polygone shape to represent the line. The line is drawn
  * relative to the world offset and rendered onto the associated window.
  */
-void DrawContext::DrawLine(Point2D from, Point2D to, float width, RGBColor c) {}
+void DrawContext::DrawLine(Point2D from, Point2D to, float width, RGBColor c) {
+    sf::ConvexShape line;
+    line.setPointCount(2);
+    line.setPoint(0, {from.x, from.y});
+    line.setPoint(1, {to.x, to.y});
+    line.setOutlineThickness(width);
+    line.setFillColor(sf::Color::Color(c.r, c.g, c.b));
+    line.setPosition({from.x, from.y});
+    mWindow->draw(line);
+}
 
 int DrawContext::GetWindowWidth() { return mWindow->getSize().x; }
 
